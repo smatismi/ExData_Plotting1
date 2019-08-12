dc4f7da51d23dae3172703d92897c8e8e0f2418c
smatismi <47929013+smatismi@users.noreply.github.com>
2019-08-12 14:42:13 -0400

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,14 @@
+#R code for generationg plot2
+
+df <- read.csv("C:/Users/smatismi/OneDrive - Capgemini/Documents/Coursera/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+#subset data to Jan and Feb 2007
+df1 <- subset(df, Date %in% c("1/2/2007","2/2/2007"))
+df1$Date <- as.Date(df1$Date, format="%d/%m/%Y")
+datetime <- paste(as.Date(df1$Date), df1$Time)
+df1$Datetime <- as.POSIXct(datetime)
+# create Plot 2
+with(df1, {plot(Global_active_power~Datetime, type="l",ylab="Global Active Power (kilowatts)", xlab="")
+})
+#send plot to file
+png("plot2.png", width=480, height=480)
+dev.off()
