6d3582c3498966884417e29019a6956f8ef809ff
smatismi <47929013+smatismi@users.noreply.github.com>
2019-08-12 14:08:51 -0400

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,10 @@
+#R code for generationg plot1
+
+df <- read.csv("C:/Users/smatismi/OneDrive - Capgemini/Documents/Coursera/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+#subset data to Jan and Feb 2007
+df1 <- subset(df, Date %in% c("1/2/2007","2/2/2007"))
+df1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+hist(df1$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+#send plot to file
+png("plot1.png", width=480, height=480)
+dev.off()
