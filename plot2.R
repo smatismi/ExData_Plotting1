5114bc6fdab602d33191a642bdfa2dce140d64b1
smatismi <47929013+smatismi@users.noreply.github.com>
2019-08-12 14:43:57 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,6 +3,7 @@
 df <- read.csv("C:/Users/smatismi/OneDrive - Capgemini/Documents/Coursera/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 #subset data to Jan and Feb 2007
 df1 <- subset(df, Date %in% c("1/2/2007","2/2/2007"))
+#convert the Date and Time variables to Date/Time classes
 df1$Date <- as.Date(df1$Date, format="%d/%m/%Y")
 datetime <- paste(as.Date(df1$Date), df1$Time)
 df1$Datetime <- as.POSIXct(datetime)
