bfacf4da597da93ba43fadecebf9e518edd8034d
smatismi <47929013+smatismi@users.noreply.github.com>
2019-08-13 08:05:03 -0400

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -10,11 +10,11 @@ df1$Datetime <- as.POSIXct(datetime)
 with(df1, {
   plot(Sub_metering_1~Datetime, type="l",
        ylab="Global Active Power (kilowatts)", xlab="")
-  lines(Sub_metering_2~Datetime,col='Red')
-  lines(Sub_metering_3~Datetime,col='Blue')
+  lines(Sub_Metering_2~Datetime,col='Red')
+  lines(Sub_Metering_3~Datetime,col='Blue')
 })
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
-       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+       legend=c("Sub_Metering_1", "Sub_Metering_2", "Sub_Metering_3"))
 #send plot to file
 png("plot3.png", width=480, height=480)
 dev.off()
