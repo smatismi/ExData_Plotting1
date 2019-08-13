35fdd79aa11b865f140c3acab698b8206f4b999c
smatismi <47929013+smatismi@users.noreply.github.com>
2019-08-13 07:57:17 -0400

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -20,9 +20,10 @@ with(df1, {
   lines(Sub_metering_3~Datetime,col='Blue')
   legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  plot(Global_Reactive_Power~Datetime, type="l", 
+  plot(Global_reactive_power~Datetime, type="l", 
        ylab="Global Reactive Power (kilowatts)",xlab="")
 })
+
 #send plot to file
 png("plot4.png", width=480, height=480)
 dev.off()
